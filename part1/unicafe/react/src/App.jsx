8c6476d0a033adf6317fd6534d569f6ab7dd4ae8
null <null>
2024-01-30 15:45:14 -0500

    exercises 1.6 and 1.7 completed

--- part1/unicafe/react/src/App.jsx
+++ part1/unicafe/react/src/App.jsx
@@ -1,33 +1,53 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
+
+const Statistics = (props) => {
+  console.log(props)
+  return (
+    <>
+    <h2>Statistics </h2>
+        <p>Good: {props.good}</p>
+        <p>Neutral: {props.neutral}</p>
+        <p>Bad: {props.bad}</p>
+        <p>All: {props.good + props.neutral + props.bad}</p>
+        <p>Average: {(props.good + props.neutral + props.bad)/3}</p>
+        <p>Positive Average: {(props.good + props.neutral) /2}</p>
+
+    
+    </>
+  )
+
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="card">
+      <h1>Give Feedback </h1>
+        <button onClick={() => setGood((good) => good + 1)}>
+          good 
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={() => setNeutral((neutral) => neutral + 1)}>
+          neutral 
+        </button>
+        <button onClick={() => setBad((bad) => bad + 1)}>
+          bad 
+        </button>
+
+        
+        
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+
+      
     </>
   )
 }
